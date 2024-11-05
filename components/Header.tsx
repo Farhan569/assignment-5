d74d4be3f2c79cbed81c23267e388c1d8ca634fe
M Farhan Ahmed <fa7727380@gmail.com>
2024-11-05 13:52:50 +0500

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link';
+
+const header = () => {
+  return (
+    <div className='bg-[#A29875] h-[134px] '>
+      <Link href='/' className='text-[75px] text-white px-9 '>MANZZARI</Link>
+    </div>
+  );
+};
+
+export default header
